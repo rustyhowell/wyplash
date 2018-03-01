19c2e74bcdfda913d3dcc60a64d06fe6c678eee9
Rusty Howell <rustyhowell@gmail.com>
2018-02-28 23:14:00 -0700

    First migration

--- app/models.py
+++ app/models.py
@@ -7,4 +7,18 @@ class TestSuite(db.Model):
     def __repr__(self):
         return '<TestSuite {}>'.format(self.name)
 
-    
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Test {}>'.format(self.name)
+
+
+class TestResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<TestResult {}>'.format(self.name)
