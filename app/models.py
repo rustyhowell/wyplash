675ef2e9f396b94fe37ff4bcc278c50c0a885f29
Rusty Howell <rustyhowell@gmail.com>
2018-02-28 23:22:56 -0700

    Add example junit xml

--- app/models.py
+++ app/models.py
@@ -11,6 +11,9 @@ class TestSuite(db.Model):
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    classname = db.Column(db.String(64), index=True, unique=True)
+    line = db.Column(db.Integer)
+    status = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Test {}>'.format(self.name)
